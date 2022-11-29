f95271ec206ee33d663fadd9ab9eb7b179eb7522
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2022-11-29 17:07:01 -0300

    update

--- Fracao.h
+++ Fracao.h
@@ -6,20 +6,20 @@ class Fracao
 private:
     int numerador;
     int denominador;
-
+    int mdc();
+    void simplifica();
+//Fazer as contas recebendo Fracao;
 public:
     Fracao();
     ~Fracao();
     void setNumerador(int numerador);
     int getNumerador() const;
-    void setDenominador(int denominador);
+    bool setDenominador(int denominador);
     int getDenominador() const;
-    void soma(int, int);
-    void subtrai(int, int);
-    void divide(int, int);
-    void multiplica(int, int);
-    int mdc();
-    void simplifica();
+    void soma(Fracao, Fracao);
+    void subtrai(Fracao, Fracao);
+    void divide(Fracao, Fracao);
+    void multiplica(Fracao, Fracao);
 };
 
 Fracao::Fracao()
@@ -44,9 +44,14 @@ int Fracao::getNumerador() const
     return numerador;
 }
 
-void Fracao::setDenominador(int denominador)
+bool Fracao::setDenominador(int denominador)
 {
+    if (denominador == 0){
+        return false;
+        throw string("Denominador nao pode ser zero");
+    }
     this->denominador = denominador;
+    return true;
 }
 
 int Fracao::getDenominador() const
@@ -54,43 +59,49 @@ int Fracao::getDenominador() const
     return denominador;
 }
 
-void Fracao::soma(int n, int d)
+void Fracao::soma(Fracao a, Fracao b)
 {
-    if (denominador == d)
+    if (a.getDenominador() == b.getDenominador())
     {
-        numerador += n;
+        numerador = a.getNumerador() + a.getDenominador();
     }
     else
     {
-        numerador = (numerador * d) + (denominador * n);
-        denominador *= d;
+        numerador = (a.getNumerador() * b.getDenominador()) + (a.getDenominador() * b.getNumerador());
+        denominador = a.getDenominador() * b.getDenominador();
     }
+    simplifica();
 }
 
-void Fracao::subtrai(int n, int d)
+void Fracao::subtrai(Fracao a, Fracao b)
 {
-    if (denominador == d)
+    if (a.getDenominador() == b.getDenominador())
     {
-        numerador -= n;
+        numerador = a.getNumerador() - b.getNumerador();
     }
     else
     {
-        numerador = (numerador * d) - (denominador * n);
-        denominador = denominador * d;
+        numerador = (a.getNumerador() * b.getDenominador()) - (a.getDenominador() * b.getNumerador());
+        denominador = a.getDenominador() * b.getDenominador();
     }
+    simplifica();
 }
 
-void Fracao::divide(int n, int d)
+void Fracao::divide(Fracao a, Fracao b)
 {
-    int num = n;
-    numerador *= d;
-    denominador *= num;
+    if (b.getNumerador() == 0){
+        throw string("Divisao por zero");
+    }
+    numerador = a.getNumerador() * b.getDenominador();
+    denominador = a.getDenominador() * b.getNumerador();
+    simplifica();
 }
 
-void Fracao::multiplica(int n, int d)
+void Fracao::multiplica(Fracao a, Fracao b)
 {
-    numerador *= n;
-    denominador *= d;
+    numerador = a.getNumerador() * b.getNumerador();
+    denominador = a.getDenominador() * b.getDenominador();
+    simplifica();
 }
 
 int Fracao::mdc()
--- TestaFracao.h
+++ TestaFracao.h
@@ -2,74 +2,82 @@
 #define TESTAFRACAO_H_
 #include "Fracao.h"
 
-class TestaFracao{
-    private:
-        Fracao f, f2;
-    public:
-        void menu();
+class TestaFracao
+{
+private:
+    Fracao f1, f2, f3;
+
+public:
+    void menu();
 };
 
-void TestaFracao::menu(){
+void TestaFracao::menu()
+{
     int opcao;
     int flag;
     int entrada;
 
-    do{
-            do{
+    do
+    {
+        try
+        {
+            do
+            {
                 cout << "Escolha uma opcao:\n";
                 cout << "1 - Soma\n";
                 cout << "2 - Subtracao\n";
                 cout << "3 - Multiplicacao\n";
                 cout << "4 - Divisao\n";
-                cout << "5 - Maximo divisor comum\n";
-                cout << "6 - Simplificar\n";
                 cin >> opcao;
-            }while (opcao <1 || opcao > 6);
+            } while (opcao < 1 || opcao > 4);
 
-            cout << "Digite o numerador fracao 1: ";
+            cout << "Digite o numerador da 1 fracao: ";
             cin >> entrada;
-            f.setNumerador(entrada);
-            cout << "Digite o denominador fracao 1: ";
-            cin >> entrada;
-            f.setDenominador(entrada);
+            f1.setNumerador(entrada);
+            do
+            {
+                cout << "Digite o denominador da 1 fracao: ";
+                cin >> entrada;
+                f1.setDenominador(entrada);
+            } while (f1.setDenominador(entrada) == false);
 
             cout << "Digite o numerador fracao 2: ";
             cin >> entrada;
             f2.setNumerador(entrada);
-            cout << "Digite o denominador fracao 2: ";
-            cin >> entrada;
-            f2.setDenominador(entrada);
+            do
+            {
+                cout << "Digite o denominador fracao 2: ";
+                cin >> entrada;
+                f2.setDenominador(entrada);
+            } while (f2.setDenominador(entrada) == false);
 
-            switch(opcao){
-                case 1:
-                    f.soma(f2.getNumerador(), f2.getDenominador());
-                    break;
-                case 2:
-                    f.subtrai(f2.getNumerador(), f2.getDenominador());
-                    break;
-                case 3:
-                    f.multiplica(f2.getNumerador(), f2.getDenominador());
-                    break;
-                case 4:
-                    f.divide(f2.getNumerador(), f2.getDenominador());
-                    break;
-                case 5:
-                    f.mdc();
-                    break;
-                case 6:
-                    f.simplifica();
-                    break;
+            switch (opcao)
+            {
+            case 1:
+                f3.soma(f1, f2);
+                break;
+            case 2:
+                f3.subtrai(f1, f2);
+                break;
+            case 3:
+                f3.multiplica(f1, f2);
+                break;
+            case 4:
+                f3.divide(f1, f2);
+                break;
             }
 
-            if (opcao == 5){
-                cout << "Maximo divisor comum: " << f.mdc() << endl;
-            }else{
-                cout << "Fracao: " << f.getNumerador() << '/' << f.getDenominador() << endl;
-            }
+            cout << "Fracao: " << f3.getNumerador() << '/' << f3.getDenominador() << endl;
+            cout << "Decimal: " << (float)f3.getNumerador() / (float)f3.getDenominador() << endl;
+        }
+        catch (string error)
+        {
+            cout << "erro: " << error;
+        }
 
         cout << "digite 0 para encerrar: ";
         cin >> flag;
-    }while(flag != 0);
+    } while (flag != 0);
 }
 
 #endif
